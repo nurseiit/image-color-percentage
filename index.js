fe61984a52fdf3e6961221ab738c904347c1af32
nurseiit <nurseyit.abdimomyn@gmail.com>
2018-09-12 23:11:53 +0900

    var-s changed to let-s

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ let inpEl = document.getElementById("myInput");
 inpEl.addEventListener("change", fileHandler, false);
 
 function fileHandler(e) {
-  var file = e.target.files[0];
-  var reader = new FileReader();
+  let file = e.target.files[0];
+  let reader = new FileReader();
   reader.addEventListener("load", main, false);
   reader.readAsArrayBuffer(file);
 }
 function main(e) {
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
+  let c = document.getElementById("myCanvas");
+  let ctx = c.getContext("2d");
 
   let buffer = e.target.result;
   let bitmap = getBMP(buffer)
@@ -19,7 +19,7 @@ function main(e) {
   c.width = width;
   c.height = height;
 
-  var img = new Image();
+  let img = new Image();
   img.src = "data:image/png;base64," + btoa([].reduce.call(new Uint8Array(buffer),function(p,c){return p+String.fromCharCode(c)},''));
   img.onload = function () {
    ctx.drawImage(img, 0, 0);
@@ -61,8 +61,8 @@ function rgbToInt (r, g, b) {
 }
 
 function getBMP(buffer) {
-  var datav = new DataView(buffer);
-  var bitmap = {};
+  let datav = new DataView(buffer);
+  let bitmap = {};
 
   bitmap.fileheader = {};
   bitmap.fileheader.bfOffBits = datav.getUint32(10, true);
@@ -72,7 +72,7 @@ function getBMP(buffer) {
   bitmap.infoheader.biHeight = datav.getUint32(22, true);
   bitmap.infoheader.biBitCount = datav.getUint16(28, true);
 
-  var start = bitmap.fileheader.bfOffBits;
+  let start = bitmap.fileheader.bfOffBits;
   bitmap.stride = Math.floor((bitmap.infoheader.biBitCount * bitmap.infoheader.biWidth + 31) / 32) * 4;
   bitmap.pixels = new Uint8Array(buffer, start);
   return bitmap;
@@ -80,13 +80,13 @@ function getBMP(buffer) {
 
 function convertToImageData(bitmap) {
   canvas = document.createElement("canvas");
-  var ctx = canvas.getContext("2d"); 
-  var Width = bitmap.infoheader.biWidth;
-  var Height = bitmap.infoheader.biHeight;
-  var imageData = ctx.createImageData(Width, Height);
-  var data = imageData.data;
-  var bmpdata = bitmap.pixels;
-  var stride = bitmap.stride;
+  let ctx = canvas.getContext("2d"); 
+  let Width = bitmap.infoheader.biWidth;
+  let Height = bitmap.infoheader.biHeight;
+  let imageData = ctx.createImageData(Width, Height);
+  let data = imageData.data;
+  let bmpdata = bitmap.pixels;
+  let stride = bitmap.stride;
 
   for (let y = 0; y < Height; ++y) {
     for (let x = 0; x < Width; ++x) {
